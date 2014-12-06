d62cb960bbfc28dd1ce6aa3b17a20a1662a0941c
atombrot <atombrot@gonzales>
2014-12-06 13:03:30 +0100

    Initial commit

---
+++ android/src/digitalmeat/ld31/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package digitalmeat.ld31.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import digitalmeat.ld31.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
